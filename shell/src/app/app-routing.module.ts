c0857f642900d4939447b7209c9ff824e2fc5c17
BPADMIN <BPADMIN@BP-WIN.boardpac.local>
2021-06-15 01:20:31 +0530

    login page and routing changes

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [
+const routes: Routes = [{
+    path:'',
+    component:LoginComponent
+  },{
+    path:'mainPage',
+    component:MainPageComponent
+  },
   {
     path: 'apponeroute',
      loadChildren: () => import('appOne/moduleappone').then(m => m.PageoneModule)
--- shell/src/app/app.module.ts
+++ shell/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WrapperComponent } from './wrapper.component';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WrapperComponent
+    WrapperComponent,
+    LoginComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
